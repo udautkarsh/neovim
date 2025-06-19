7cebeeed1601d858954a5212d115237ade74740f
Uday Singh <uday.singh@penguinsolutions.com>
2025-06-19 15:32:57 +0530

    snack: dashboard

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -35,13 +35,13 @@ return {
     require("nvim-tree").setup(opts)
 
     -- Auto-open nvim-tree when Neovim starts without a file
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        if vim.fn.argc() == 0 then
-          require("nvim-tree.api").tree.open()
-        end
-      end
-    })
+    -- vim.api.nvim_create_autocmd("VimEnter", {
+    --  callback = function()
+    --    if vim.fn.argc() == 0 then
+    --      require("nvim-tree.api").tree.open()
+    --   end
+    --  end
+    --})
 
     -- Ensure nvim-web-devicons is properly configured
     require("nvim-web-devicons").setup({
