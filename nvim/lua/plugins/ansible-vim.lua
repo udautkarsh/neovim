bedc21ecb7fa92c227ab87cb3fc08414efa29025
uday singh <uday.singh@penguinsolutions.com>
2025-07-16 16:11:20 +0530

    working

--- nvim/lua/plugins/ansible-vim.lua
+++
@@ -1,8 +0,0 @@
--- ansible-vim plugin configuration
--- This plugin provides syntax highlighting, indentation, and other features for Ansible files in Vim.
-
-return {
-    "pearofducks/ansible-vim",
-    lazy = false,
-    ft = { "yaml.ansible", "ansible" }, -- Only load for Ansible/YAML files
-  }
